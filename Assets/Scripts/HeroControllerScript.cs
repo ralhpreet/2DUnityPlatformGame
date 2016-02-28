e9f060e91e1c397026a617a195f3301667b72c77
ralhpreet <preetralh07@gmail.com>
2016-02-28 01:02:29 -0500

    made hero able to walk and jump

---
+++ Assets/Scripts/HeroControllerScript.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Velocity range utility class#######
+[System.Serializable]
+public class VelocityRange
+{
+    //Public Instance Variable
+    public float minimum;
+    public float maximum;
+
+    //Constructor#####
+    public VelocityRange(float minimum, float maximum)
+    {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+}
+
+public class HeroControllerScript : MonoBehaviour
+{
+
+    //Public Instance variables
+    public VelocityRange velocityRange;
+    public float moveForce;
+    public float jumpForce;
+    public Transform groundCheck;
+    public Transform camera;
+
+    //public GameController gameController;
+
+
+    //Private Instance Variables
+    private Animator _animator;
+    private float _move;
+    private float _jump;
+    private bool _facingRight;
+    private Transform _transform;
+    private Rigidbody2D _rigidBody2D;
+    private bool _isGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initialize Public Variables
+        this.velocityRange = new VelocityRange(300f, 2000f);
+        // this.moveForce = 50f;
+        //this.jumpForce = 500f;
+        //Initialize Private Varibles 
+        this._transform = gameObject.GetComponent<Transform>();
+        this._animator = gameObject.GetComponent<Animator>();
+        this._rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        this._move = 0f;
+        this._jump = 0f;
+        //set default animation  to "idle"
+        //this._animator.SetInteger("AnimState", 0);
+        this._facingRight = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       // this._isGrounded = true;
+        
+        this._isGrounded = Physics2D.Linecast(this._transform.position, this.groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.DrawLine(this._transform.position, this.groundCheck.position);
+        float forceX = 0f;
+        float forceY = 0f;
+
+        //ge absolute value of velocity for our gameobject
+        float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
+        float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
+        Debug.Log(_isGrounded);
+        //check if the player is grounded
+        if (this._isGrounded)
+        {
+            //gets a number between -1 to 1 for both horizontaland vertical axis
+            this._move = Input.GetAxis("Horizontal");
+            this._jump = Input.GetAxis("Vertical");
+            if (this._move != 0)
+            {
+                if (this._move > 0)
+                {
+                    //movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = this.moveForce;
+                    }
+                    this._facingRight = true;
+                    this._flip();
+                }
+                if (this._move < 0)
+                {
+                    //movement force
+                    if (absVelX < this.velocityRange.maximum)
+                    {
+                        forceX = -this.moveForce;
+                    }
+                    this._facingRight = false;
+                    this._flip();
+                }
+                //call the walk animation
+                this._animator.SetInteger("AnimState", 1);
+            }
+            else
+            {
+                //set to idle
+                this._animator.SetInteger("AnimState", 0);
+            }
+            if (this._jump > 0)
+            {
+                //jump force
+                if (absVelY < this.velocityRange.maximum)
+                {
+                    forceY = this.jumpForce;
+                }
+
+            }
+        }
+        else
+        {
+            //call the jump animation
+            this._animator.SetInteger("AnimState", 2);
+        }
+        //Apply forces to the player
+        this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
+    }
+
+    //Private Methods
+
+    private void _flip()
+    {
+        if (this._facingRight)
+        {
+            this._transform.localScale = new Vector2(2.05f, 1.6f);
+        }
+        else
+        {
+            this._transform.localScale = new Vector2(-2.05f, 1.6f);
+        }
+    }
+}
